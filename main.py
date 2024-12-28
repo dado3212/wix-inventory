49e13e22d461b358acfefd7b6e7226d58a8dcaa3
Alex Beals <alexcbeals@gmail.com>
2024-12-27 21:19:23 -0800

    Functionally working script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from secret import API_KEY, ACCOUNT_ID, SITE_ID
-import requests, json
+import requests, json, time
 
-PRINTS_COLLECTION = '2102ce4e-5949-5f58-4b2c-40688ecd5cb3'
+PRINTS_COLLECTION = 'fa016b57-2546-b7cc-8a30-743695fe1f5e'
 
 '''
 Create a new product. There will be a base image.
@@ -58,18 +58,8 @@ def createProduct():
     
     # And setup the variants
     productId = response.json()['product']['id']
-    print(productId)
-
-    variantIds = getVariantIDs(productId)
-    print(variantIds)
-    updateInventory(productId, variantIds)
     
-    # Add to prints collection
-    addToPrints(productId)
-
-
-def getProducts():
-    url = "https://www.wixapis.com/stores/v1/products/query"
+    url = f"https://www.wixapis.com/stores/v1/products/{productId}/variants"
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json, text/plain, */*",
@@ -77,20 +67,27 @@ def getProducts():
         "wix-site-id": SITE_ID,
     }
     data = {
-        "query": {
-            # "filter": "{\"paymentStatus\":\"PAID\"}",
-            # "sort": "{\"number\": \"desc\"}",
-            "paging": {
-                "limit": "50"
-            }
-        }
+        "variants": [{"choices": {"Size": x}, "price": products[x]} for x in products.keys()],
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.patch(url, headers=headers, json=data)
+        
+    print('Waiting 5 seconds for product to become available for editing...')
+    time.sleep(5)
 
-    print(response.status_code)
-    print(response.json())
+    variantIds = getVariantIDs(productId)
+    if updateInventory(productId, variantIds):
+        print('Updated inventory.')
+    else:
+        print('Something went wrong updating variant prices.')
     
+    # Add to prints collection
+    if addToPrints(productId):
+        print('Added to prints.')
+    else:
+        print('Something went wrong added to prints.')
+    print(f'\n\nClick this link to finish setting it up: https://manage.wix.com/dashboard/{SITE_ID}/store/products/product/{productId}')
+
 def addToPrints(productId):
     url = f"https://www.wixapis.com/stores/v1/collections/{PRINTS_COLLECTION}/productIds"
     headers = {
@@ -105,7 +102,8 @@ def addToPrints(productId):
         ]
     }
 
-    response = requests.patch(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data)
+    return response.status_code == 200
     
 def updateInventory(productId, variant_ids):
     url = f"https://www.wixapis.com/stores/v2/inventoryItems/product/{productId}"
@@ -123,7 +121,11 @@ def updateInventory(productId, variant_ids):
     }
 
     response = requests.patch(url, headers=headers, json=data)
-    print(response.json())
+    if (response.status_code == 200):
+        return True
+    else:
+        print(response.json())
+        return False
     
 def getVariantIDs(productId):
     url = "https://www.wixapis.com/stores-reader/v2/inventoryItems/query"
@@ -135,7 +137,7 @@ def getVariantIDs(productId):
     }
     data = {
         "query": {
-            "filter": json.dumps({'productId': productId}),
+            "filter": "{\"productId\": \"" + productId + "\"}",
             "paging": {
                 "limit": "50"
             }
@@ -143,7 +145,6 @@ def getVariantIDs(productId):
     }
 
     response = requests.post(url, headers=headers, json=data)
-    print(response.json())
     
     if (response.status_code != 200):
         print("Failed to find item.")
@@ -156,9 +157,4 @@ def getVariantIDs(productId):
     
     return [x['variantId'] for x in data[0]['variants']]
 
-# getInventory()
-# product = '47b33cd2-57fc-40f5-9466-d2b757b07894'
-# variant_ids = getVariantIDs('47b33cd2-57fc-40f5-9466-d2b757b07894')
-# updateInventory(product, variant_ids)
-# # updateInventory(
 createProduct()
